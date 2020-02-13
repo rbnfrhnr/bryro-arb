3e65ab27df07474b94ac998342e39ce353300d5d
rbnfrhnr <frehner.robin@gmail.com>
2020-02-13 22:28:29 +0100

    - db job

--- db/sql/v1.0.0/v1_0_0_create_order_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [IF NOT EXISTS] TBL_ORDER(
-  id int AUTO_INCREMENT PRIMARY KEY,
-  order_type varchar(50) not null,
-  price double not null,
-  quantity double not null,
-  exchange varchar(50) not null,
-  created_at TIMESTAMP not null,
-  coin_pair varchar(50) not null
-)
