feb0623bb79348a034c8cf2d18f1415c2a529a23
frehnrob <frehner.robin@gmail.com>
2020-04-02 10:08:48 +0200

    - pipe channels

--- exchange/src/Exchange/Network/Sync/Cluster.hs
+++ exchange/src/Exchange/Network/Sync/Cluster.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Exchange.Network.Sync.Cluster (
        test2
 ) where
@@ -18,143 +19,111 @@ import System.IO
 import Exchange.Kraken.Utils as K
 import Exchange.Bitstamp.Utils as BIT
 
-type ChainedChannel = IO ()
+data TransformerData a = TransformerData a deriving (Show)
 
-{- | Maybe ByteString determines whether a message needs to be sent or not. The map serves as a lookup table -}
-data UniqueTransformer = UniqueTransformer (Maybe BL.ByteString) (Map.Map BL.ByteString BL.ByteString) deriving (Show)
-
-{- | Makes use of the Unique transformer. If the message is set we will write to the sink and add the message to the lookup table -}
-writeUnique :: Chan.Chan BL.ByteString -> UniqueTransformer -> IO (UniqueTransformer)
-writeUnique chan (UniqueTransformer (Just msg) lookupMap) = Chan.writeChan chan msg >> (return $ UniqueTransformer Nothing (Map.insert msg msg lookupMap))
-writeUnique chan (UniqueTransformer _ lookupMap)          = return (UniqueTransformer Nothing lookupMap)
-
-{- | Will tell whether this message is unique or not. lookupFn should be a partially applied function which solely depends on the msg -}
-applyUniqueFilterSink :: Chan.Chan BL.ByteString -> (BL.ByteString -> UniqueTransformer) -> IO (UniqueTransformer)
-applyUniqueFilterSink source lookupFn = (liftM lookupFn (Chan.readChan source))
-
-{- | Pipes messages from a source to a sink and makes sure that messages are not redundant -}
-uniqueCChan :: Chan.Chan BL.ByteString -> Chan.Chan BL.ByteString -> UniqueTransformer -> IO (UniqueTransformer)
-uniqueCChan source sink transformer = (applyUniqueFilterSink source (filterPresent transformer)) >>= (writeUnique sink)
-
-runUniqueCChan :: IO (UniqueTransformer) -> IO ()
-runUniqueCChan uniquePipe = uniquePipe >> runUniqueCChan uniquePipe
-
-{- | is responsible for determining whether the message has already be sent or not. -}
-filterPresent :: UniqueTransformer -> BL.ByteString -> UniqueTransformer
-filterPresent (UniqueTransformer _ lookupMap) bs = case Map.lookup bs lookupMap of
-                                                   (Just _) -> (UniqueTransformer Nothing lookupMap)
-                                                   Nothing  -> (UniqueTransformer (Just bs) lookupMap)
-
-{- | Creates a simple pipeline forwarding messages from a source to a sink -}
-chainChannel :: Chan.Chan BL.ByteString -> Chan.Chan BL.ByteString -> ChainedChannel
-chainChannel source sink = (Chan.readChan source) >>= (Chan.writeChan sink)
-
-{- | simple wrapper which can be run asynchronously to always pipe forward -}
-runCChan :: ChainedChannel -> IO ()
-runCChan cChan = cChan >> runCChan cChan
-
-
-{-
-for ghci
-source <- newChan :: IO (Chan ByteString)
-sink <- newChan :: IO (Chan ByteString)
-let mp = M.empty :: M.Map ByteString ByteString
-writeChan source "test4"
-let uniquePipe = uniqueCChan source sink (UniqueTransformer Nothing M.empty)
+{- | takes the TransformerData buffer a Chan and allows you to write to the output channel (not mandatory).
+     returns a new TransformerData state
 -}
+class WriteOut a b where
+    writeOut :: Chan b -> TransformerData a  -> IO (TransformerData a)
 
---dummyLiveReload :: IO
---dummyLiveReload = do
---        {- this is our initial setting once we live reload-}
---
---        currentMsgInChan <- Chan.newChan :: IO (Chan BL.ByteString) -- current messages from websockets
---        bufferChan <- Chan.newChan :: IO (Chan BL.ByteString) -- man in the middle
---        outChan <- Chan.newChan :: IO (Chan BL.ByteString) -- propagates to cluster subscribers
---
---        let fromInToBuffer = chainChannel currentMsgInChan bufferChan
---        let fromBufferToOut = chainChannel bufferChan outChan
---
---        ibTid <- forkIO runCChan fromInToBuffer -- forward everything to buffer
---        boTid <- forkIO runCChan fromBufferToOut -- forward everything to out
---        {- start live reload -}
---
---        newMsgInChan <- Chan.newChan :: IO (Chan BL.ByteString) -- this is the new channel from the new sockets
---
---        let fromNewInToBuffer = chainChannel newMsgInChan bufferChan
---
---        nibTid <- forkIO runCChan fromNewInToBuffer -- connect to the buffer with the new messages as well
---
---        killThread boTid -- kill output stream to subscribers of cluster
---        {- now the buffer is filling with values from the old websockets and the new ones -}
---
---        {- now we need to use the unique transformer. I.e make sure we don't output the same values twice -}
---
+{- | Typeclass for transforming an incoming message. Acts as a buffer before writing to the output channel. -}
+class UpdateTransformerState a t where
+    updateTState :: TransformerData t -> a -> IO (TransformerData t)
 
 
+{- | Pipes messages from a source to a sink and transforms the data in-between -}
+filteredChan :: (WriteOut t o, UpdateTransformerState i t) => Chan.Chan i -> Chan.Chan o -> TransformerData t -> IO (TransformerData t)
+filteredChan source sink tdState = (Chan.readChan source) >>= (updateTState tdState) >>= (writeOut sink) >>= (filteredChan source sink)
+
+{- | Runs a filtered channel composite. Run it in a thread or otherwise it will block -}
+runFilteredChan :: IO (TransformerData t) -> IO ()
+runFilteredChan fChan = fChan >> runFilteredChan fChan
 
 
+{- | Simple data structure for deduplication of messages in a Chan -}
+data DeduplicationTState = DeduplicationTState (Maybe BL.ByteString) (Map.Map BL.ByteString BL.ByteString) deriving (Show)
+
+instance WriteOut DeduplicationTState BL.ByteString where
+    writeOut chan (TransformerData (DeduplicationTState (Just msg) lookupMap)) = Chan.writeChan chan msg >> return (TransformerData $ DeduplicationTState Nothing (Map.insert msg msg lookupMap))
+    writeOut chan (TransformerData (DeduplicationTState _ lookupMap))          = return $ TransformerData $ DeduplicationTState Nothing lookupMap
+
+instance UpdateTransformerState BL.ByteString DeduplicationTState where
+    updateTState (TransformerData (DeduplicationTState _ lookupMap)) bs = case Map.lookup bs lookupMap of
+                                                                   (Just _) -> return $ TransformerData $ DeduplicationTState Nothing lookupMap
+                                                                   Nothing  -> return $ TransformerData $ DeduplicationTState (Just bs) lookupMap
+
+pipeChan :: Chan.Chan a -> Chan.Chan a -> IO ()
+pipeChan source sink = (Chan.readChan source) >>= (Chan.writeChan sink)
+
+runPipedChan :: IO () -> IO ()
+runPipedChan pipedChan = pipedChan >> runPipedChan pipedChan
 
 
+--currentMsgInChan <- Chan.newChan :: IO (Chan BL.ByteString) -- current messages from websockets
+--bufferChan <- Chan.newChan :: IO (Chan BL.ByteString) -- man in the middle
+--outChan <- Chan.newChan :: IO (Chan BL.ByteString) -- propagates to cluster subscribers
 
-
-
-syncMessages :: Chan.Chan BL.ByteString -> Chan.Chan BL.ByteString -> Chan.Chan BL.ByteString -> Map.Map BL.ByteString BL.ByteString -> IO ()
-syncMessages oldChan newChan bufferChan msgMap = do
-    putStrLn "syncing..." >> (hFlush stdout)
-    msgFromOld <- Chan.readChan oldChan
-    msgMap <- handleMessage bufferChan msgFromOld msgMap
-    msgFromNew <- Chan.readChan newChan
-    msgMap <- handleMessage bufferChan msgFromNew msgMap
-    syncMessages oldChan newChan bufferChan msgMap
-
-handleMessage :: Chan.Chan BL.ByteString -> BL.ByteString -> Map.Map BL.ByteString BL.ByteString -> IO (Map.Map BL.ByteString BL.ByteString)
-handleMessage bufferChan msg msgMap = case Map.lookup msg msgMap of
-                                      Nothing  -> do
-                                                  Chan.writeChan bufferChan msg
-                                                  return $ Map.insert msg msg msgMap
-                                      (Just _) -> return msgMap
 
 
 liveReconnect :: SyncedWSCluster -> IO (SyncedWSCluster)
-liveReconnect cluster@(SyncedWSCluster outChannel inChannel nodeMap spid) = do
+liveReconnect cluster@(SyncedWSCluster outChannel inChannel nodeMap oldChannelToOutTid) = do
     bufferChan <- Chan.newChan -- will be our new channel to the nodes
-    tempChannel <- Chan.newChan
+    newChan <- Chan.newChan -- the new channel for the WebSockets to send data to
 
-    smPid <- forkIO $ syncMessages inChannel tempChannel bufferChan $ Map.empty
+    {- kill the stream from the webSockets to retire chan to the out chan. (messages will now queue in inChannel) -}
+    killThread oldChannelToOutTid
 
+    let bufferToOUt = filteredChan bufferChan outChannel (TransformerData (DeduplicationTState Nothing Map.empty))
+
+    {- sync buffer to out (deduplication)-}
+    bTid <- forkIO $ runFilteredChan bufferToOUt
+    putStrLn "STARTED DEDUPLICATION !!!!!!!!! \n\n\n\n\n"
+
+    {- redirect data from webSockets to retire to deduplication queue (buffer to out) -}
+    oldChannelToBufferTid <- forkIO $ runPipedChan $ pipeChan inChannel bufferChan
+
+    {- redirect data from webSockets to create to deduplication queue (buffer to out) -}
+    newChannelToBufferTid <- forkIO $ runPipedChan $ pipeChan newChan bufferChan
+
+    {- create the new WebSockets and their respective worker nodes -}
     newNodes <- foldM (\mp node@(SyncedWSNode _ name _ _ _ _) -> do
-                                                               nn <- (mapToNewNodes node bufferChan)
+                                                               nn <- (mapToNewNodes node newChan)
                                                                return (Map.insert name nn mp)  ) Map.empty nodeMap
 
---    newNodes <- foldM (\mp node@(SyncedWSNode _ name _ _ _) -> liftM ((mapToNewNodes node bufferChan) >>= (Map.insert name)) mp ) Map.empty nodeMap
-    {- we synchronize the old and new values and keep them unique -}
+    {- Disconnect the old WebSockets and close kill threads  -}
     _ <- mapM (\(SyncedWSNode pid _ _ _ _ cmdIn) -> do (MVar.putMVar cmdIn Exit)) nodeMap
+
     {- todo NOW WE HAVE TO WAIT UNTIL THE NEW SOCKETS ARE CONNECTED... THEN KILL THE OLD SYNC-}
-    putStrLn "before killing"
-    smth <- killThread spid -- kill the synchronizer
-    putStrLn $ show smth
-    putStrLn "after kiling sync spid"
-    threadDelay 5000000
-    putStrLn "afterDelay"
+    threadDelay (4 * 1000 * 1000)
 
+    {- kill the old pipe from the now retired/killed WebSockets to the buffer/deduplicator-}
+    killThread oldChannelToBufferTid
 
-    newSpid <-forkIO $ syncQueues bufferChan outChannel
-    {-todo now we need to clean up the retired websocket threads -}
-    return (SyncedWSCluster outChannel bufferChan newNodes newSpid)
+    {- kill the deduplicator pipe since there is no need anymore (old WebSockets are retired/killed). messages are now queue in new chan-}
+    killThread newChannelToBufferTid
+
+    {- kill deduplicator to out-}
+    killThread bTid
+
+    putStrLn "FINISHED DEDUPLICATION !!!!!!!\n\n\n\n"
+
+    {- now pipe newChan directly to the out chan -}
+    newChannelToOutTid <- forkIO $ runPipedChan $ pipeChan newChan outChannel
+
+    return (SyncedWSCluster outChannel newChan newNodes newChannelToOutTid)
 --    let msgBuffer = Map.empty -- will be our buffer while we reconnect to websockets
 --
 mapToNewNodes :: SyncedWSNode -> Chan.Chan BL.ByteString -> IO (SyncedWSNode)
 mapToNewNodes (SyncedWSNode _ _ _ _ (WSMinimalDefinition host path port onOpen) _) chan = runSocket (WSDefinition host path port chan onOpen)
 mapToNewNodes (SyncedWSNode _ _ _ _ (WSDefinition host path port _ onOpen) _) chan = runSocket (WSDefinition host path port chan onOpen)
 
-syncQueues :: Chan.Chan BL.ByteString -> Chan.Chan BL.ByteString -> IO ()
-syncQueues inc out = (Chan.readChan inc) >>= (Chan.writeChan out) >> syncQueues inc out
 
 mkEmptyCluster :: IO (SyncedWSCluster)
 mkEmptyCluster = do
                  inc <- Chan.newChan
                  outc <- Chan.newChan
-                 spid <- forkIO $ syncQueues inc outc
+                 spid <- forkIO $ runPipedChan $ pipeChan inc outc
                  return $ SyncedWSCluster outc inc (Map.empty) spid
 --
 addSockets :: WSDefinition -> SyncedWSCluster -> IO (SyncedWSCluster)
@@ -184,6 +153,6 @@ test2 = do
        putStrLn $ show $ fmap syncedWSPID (Map.lookup "ws.bitstamp.net" (clusterWSMap cluster))
        let loop queue = do
                         msg <- Chan.readChan queue
-                        putStrLn $ show $ length $ show msg
+                        putStrLn $ show msg
                         loop queue
        loop (clusterOutChannel cluster)
