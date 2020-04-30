127350db5ffaec870f0c4a83d69e2e5022bc9eef
frehnrob <frehner.robin@gmail.com>
2020-04-30 22:17:52 +0200

    - created state like data structure for ticker transformation - created typeclass for destinations of (where to write after transformation) - CLEANUP NECESSARY!

--- ticker-feed/app/Main.hs
+++ ticker-feed/app/Main.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE OverloadedStrings         #-}
 
 module Main where
 
@@ -42,43 +42,51 @@ withConfig (Right cfg) = do
   Bitstamp.subscribeHandler $ decodeAndEnQueueHandler Bitstamp.parseToOrder orderQueue
   Kraken.subscribeHandler $ decodeAndEnQueueHandler Kraken.parseToOrder orderQueue
   Binance.subscribeHandler $ decodeAndEnQueueHandler Binance.parseToOrder orderQueue
-  runTransform orderQueue (writeKafkaState kafkaState "bryro-ticker" 0) Map.empty Map.empty
+  runTransform
+    (TickerST
+       Map.empty
+       Map.empty
+       [Destination SimpleOut, Destination (writeKafkaState kafkaState "bryro-ticker" 0)]
+       orderQueue)
 
-runTransform :: Chan.Chan [Order] -> WriteKafka -> TickBuffer -> DBookMap -> IO ()
-runTransform queue wKafka tickBuffer dBookMap =
-  Chan.readChan queue >>= return . foldl handleDepthBook dBookMap >>=
-  (\bookMap ->
+runTransform :: TickerST -> IO ()
+runTransform tickerST@(TickerST dBookMap tBuffer _ queue) =
+  Chan.readChan queue >>= return . foldl handleDepthBook tickerST >>=
+  (\tickerST ->
      foldM
-       (\(_, tBuffer) key ->
-          bufferedWrite wKafka (Map.lookup key tickBuffer) (Map.lookup key dBookMap) >>=
-          (\nTick -> return (bookMap, Map.insert key nTick tBuffer)))
-       (bookMap, tickBuffer)
-       (Map.keys bookMap)) >>=
-  (\(bookM, tiBuffer) -> runTransform queue wKafka tiBuffer bookM) >>
+       (\tst key -> bufferedWrite tst key (Map.lookup key (tickerBuffer tst)) (Map.lookup key (tickerDBookMap tickerST)))
+       tickerST
+       (Map.keys (tickerDBookMap tickerST))) >>=
+  runTransform >>
   return ()
 
-handleDepthBook :: DBookMap -> Order -> DBookMap
-handleDepthBook dBookMap order =
-  case Map.lookup currencyExchangeKey dBookMap of
-    Just depthBook -> Map.insert currencyExchangeKey (updateDepthBook depthBook order) dBookMap
-    Nothing -> handleDepthBook (Map.insert currencyExchangeKey (openDepthBook (getCurrencyPair order)) dBookMap) order
+handleDepthBook :: TickerST -> Order -> TickerST
+handleDepthBook tickerST@(TickerST dBookMap _ _ _) order =
+  case Map.lookup dBookMapKey dBookMap of
+    Just depthBook -> tickerST {tickerDBookMap = Map.insert dBookMapKey (updateDepthBook depthBook order) dBookMap}
+    Nothing ->
+      handleDepthBook
+        tickerST {tickerDBookMap = Map.insert dBookMapKey (openDepthBook (getCurrencyPair order)) dBookMap}
+        order
   where
-    currencyExchangeKey = toCurrencyExchangeKey order
+    dBookMapKey = toCurrencyExchangeKey order
 
-bufferedWrite :: WriteKafka -> Maybe (Maybe Order, Maybe Order) -> Maybe DepthBook -> IO (Maybe Order, Maybe Order)
-bufferedWrite wKafka (Just (lastAsk, lastBid)) (Just book)
-  | (lastAsk, lastBid) /= tickPair = Kafka.writeToKafka (\_ -> return ()) wKafka tickPair >> return tickPair
-  | otherwise = return (lastAsk, lastBid)
+bufferedWrite :: TickerST -> CurrencyExchangeKey -> Maybe (Maybe Order, Maybe Order) -> Maybe DepthBook -> IO TickerST
+bufferedWrite tickerST@(TickerST dbookMap tickBuffer dest queue) key (Just (lastAsk, lastBid)) (Just book)
+  | (lastAsk, lastBid) /= tickPair =
+    writeOutIO dest tickPair >>= (\uDest -> return (TickerST dbookMap (Map.insert key tickPair tickBuffer) uDest queue))
+  | otherwise = return tickerST
   where
     minAsk = fmap snd (Map.lookupMin (depthBookAsk book))
     maxBid = fmap snd (Map.lookupMax (depthBookBid book))
     tickPair = (minAsk, maxBid)
-bufferedWrite wKafka Nothing (Just book) = Kafka.writeToKafka (\msg -> return ()) wKafka tickPair >> return tickPair
+bufferedWrite tickerST@(TickerST dbookMap tickBuffer dest queue) key Nothing (Just book) =
+  writeOutIO dest tickPair >>= (\uDest -> return (TickerST dbookMap (Map.insert key tickPair tickBuffer) uDest queue))
   where
     minAsk = fmap snd (Map.lookupMin (depthBookAsk book))
     maxBid = fmap snd (Map.lookupMax (depthBookBid book))
     tickPair = (minAsk, maxBid)
-bufferedWrite wKafka lastTick book = return (Nothing, Nothing)
+bufferedWrite tickerST key lastTick book = return tickerST
 
 decodeOrders :: Either KafkaClientError [BS.ByteString] -> IO [Order]
 decodeOrders (Right msg) = return (foldl filteredDecode [] msg) :: IO [Order]
--- ticker-feed/src/Ticker/Utils.hs
+++ ticker-feed/src/Ticker/Utils.hs
@@ -1,32 +1,76 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE OverloadedStrings         #-}
 
 module Ticker.Utils
   ( CurrencyExchangeKey
   , DBookMap
+  , Destination(..)
   , TickBuffer
+  , TickerST(..)
+  , WriteOutIO
+  , SimpleOut(..)
   , printTickFiltered
   , printLowestAsk
   , toCurrencyExchangeKey
+  , writeOutIO
   ) where
 
-import           Data.Aeson
+import qualified Control.Concurrent.Chan as C
 import qualified Data.ByteString.Lazy    as BL
 import qualified Data.Map                as Map
+
+import           Control.Monad
+import           Data.Aeson
+import           Data.Time               (UTCTime)
+import           Data.Time.Clock.POSIX   (posixSecondsToUTCTime)
+import           Database.InfluxDB.Line
 import           Finance.Depthbook.Types
 import           Finance.Types
 import           Network.Kafka
 import           Network.Kafka.Producer
 import           System.IO
+import           Utils.Influx            as Influx
 import           Utils.Kafka             as Kafka
 
 instance KafkaData (Maybe Order, Maybe Order) where
   toKafkaData tick = makeMessage $ BL.toStrict $ encode tick
+
+instance WriteOutIO Kafka.WriteKafka where
+  writeOutIO wKafka tick = Kafka.writeToKafka (\msg -> return ()) wKafka tick >> return wKafka :: IO (Kafka.WriteKafka)
+
+--instance WriteOutIO Influx.InfluxConnection where
+--  writeOutIO influxCon tick = Influx.writeToInflux influxCon tick
+instance WriteOutIO SimpleOut where
+  writeOutIO simple tick = print tick >> hFlush stdout >> return SimpleOut
+
+instance WriteOutIO Destination where
+  writeOutIO (Destination desti) tick = Destination <$> writeOutIO desti tick
+
+instance WriteOutIO [Destination] where
+  writeOutIO destinations tick =
+    foldM (\list (Destination dest) -> liftM (: list) (Destination <$> writeOutIO dest tick)) [] destinations
 
 type CurrencyExchangeKey = String
 
 type DBookMap = Map.Map CurrencyExchangeKey DepthBook
 
 type TickBuffer = Map.Map CurrencyExchangeKey (Maybe Order, Maybe Order)
+
+data SimpleOut =
+  SimpleOut
+
+data Destination =
+  forall a. WriteOutIO a =>
+            Destination a
+
+data TickerST =
+  TickerST
+    { tickerDBookMap     :: !DBookMap
+    , tickerBuffer       :: !TickBuffer
+    , tickerDestinations :: ![Destination]
+    , tickerOrderQueue   :: C.Chan [Order]
+    }
 
 toCurrencyExchangeKey :: Order -> String
 toCurrencyExchangeKey order = show (getCurrencyPair order) ++ show (getExchangeFromOrder order)
@@ -55,3 +99,6 @@ printTick (Just ask, Just bid) =
      show (getCurrencyPair ask) ++ " - " ++ show (getPriceFromOrder ask) ++ " / " ++ show (getPriceFromOrder bid)) >>
   hFlush stdout
 printTick _ = return ()
+
+class WriteOutIO a where
+  writeOutIO :: a -> (Maybe Order, Maybe Order) -> IO a
