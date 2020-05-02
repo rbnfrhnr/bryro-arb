6880e047e406923ecefadb569f4fbb0118921eb8
frehnrob <frehner.robin@gmail.com>
2020-05-02 13:57:40 +0200

    - Added QuickCheck

--- finance/test/Spec.hs
+++ finance/test/Spec.hs
@@ -1,2 +1,13 @@
+import           Finance.Types
+import           Test.QuickCheck
+import           Test.QuickCheck.Gen (choose)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = verboseCheck checkMyTest
+
+checkMyTest :: BaseOrder -> Bool
+checkMyTest (BaseOrder exc cur price qty ts) =
+  exc == Bitstamp && cur == LTCUSD && (price >= 9 && price <= 10) && (qty >= 0 && qty <= 10)
+
+instance Arbitrary BaseOrder where
+  arbitrary = BaseOrder <$> return Bitstamp <*> return LTCUSD <*> choose (9, 10) <*> choose (0, 10) <*> return 0
