a367995a920b466cdba298a2bac11b2395541560
frehnrob <frehner.robin@gmail.com>
2020-05-11 19:30:27 +0200

    - cleanup

--- utils/src/Utils/Forward.hs
+++ utils/src/Utils/Forward.hs
@@ -35,13 +35,13 @@ instance (Show a) => WriteOutIO SimpleOut a where
   writeOutIO SimpleOut payload = print payload >> return SimpleOut
 
 {- | writeOut can be applied to any Destination holding a value of instance WriteOut -}
-instance (WriteOutIO (Destination b) b) => WriteOutIO (Destination b) b where
+instance WriteOutIO (Destination b) b where
   writeOutIO (Destination dest) payload = Destination <$> writeOutIO dest payload
 
 {- | writeOut can be applied to a list of Destinations holding a value of instance WriteOut
   This will allow for heterogeneous lists to be treated the same way as a single Destination or any other data type
   having a WriteOut instance
 -}
-instance (WriteOutIO (Destination b) b) => WriteOutIO [Destination b] b where
+instance WriteOutIO [Destination b] b where
   writeOutIO destinations payload =
     foldM (\list (Destination dest) -> fmap (: list) (Destination <$> writeOutIO dest payload)) [] destinations
